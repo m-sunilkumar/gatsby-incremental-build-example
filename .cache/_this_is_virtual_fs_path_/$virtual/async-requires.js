3cbc7294d9087997b7b2fc52748a03a6727f480f
sunil_kumar_060 <sunil.m@mindtree.com>
2021-04-08 13:02:54 +0530

    update

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -3,7 +3,7 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-about-js": () => import("./../../../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-home-js": () => import("./../../../src/pages/home.js" /* webpackChunkName: "component---src-pages-home-js" */)
+  "component---src-pages-home-js": () => import("./../../../src/pages/home.js" /* webpackChunkName: "component---src-pages-home-js" */),
+  "component---src-pages-menu-js": () => import("./../../../src/pages/menu.js" /* webpackChunkName: "component---src-pages-menu-js" */)
 }
 
--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\M1061879\\OneDrive - Mindtree Limited\\Documents\\gatsby-incremental-build-example\\.cache\\dev-404-page.js")),
-  "component---src-pages-about-js": preferDefault(require("C:\\Users\\M1061879\\OneDrive - Mindtree Limited\\Documents\\gatsby-incremental-build-example\\src\\pages\\about.js")),
-  "component---src-pages-home-js": preferDefault(require("C:\\Users\\M1061879\\OneDrive - Mindtree Limited\\Documents\\gatsby-incremental-build-example\\src\\pages\\home.js"))
+  "component---src-pages-home-js": preferDefault(require("C:\\Users\\M1061879\\OneDrive - Mindtree Limited\\Documents\\gatsby-incremental-build-example\\src\\pages\\home.js")),
+  "component---src-pages-menu-js": preferDefault(require("C:\\Users\\M1061879\\OneDrive - Mindtree Limited\\Documents\\gatsby-incremental-build-example\\src\\pages\\menu.js"))
 }
 
