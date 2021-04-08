9ab0497579630ec158a0804477a36c1380880dbb
sunil_kumar_060 <sunil.m@mindtree.com>
2021-04-08 10:50:06 +0530

    added build folder

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\M1061879\\OneDrive - Mindtree Limited\\Documents\\gatsby-incremental-build-example\\.cache\\dev-404-page.js")),
+  "component---src-pages-about-js": preferDefault(require("C:\\Users\\M1061879\\OneDrive - Mindtree Limited\\Documents\\gatsby-incremental-build-example\\src\\pages\\about.js")),
+  "component---src-pages-home-js": preferDefault(require("C:\\Users\\M1061879\\OneDrive - Mindtree Limited\\Documents\\gatsby-incremental-build-example\\src\\pages\\home.js"))
+}
+
---
+++ .cache/async-requires.js
@@ -0,0 +1,9 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-about-js": () => import("./../../../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-home-js": () => import("./../../../src/pages/home.js" /* webpackChunkName: "component---src-pages-home-js" */)
+}
+
