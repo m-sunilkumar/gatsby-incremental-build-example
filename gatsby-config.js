1945e8df38e2e56ef6055ba50389d558f8cd2a22
sunil_kumar_060 <sunil.m@mindtree.com>
2021-04-08 12:54:38 +0530

    contentfull addedd

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,27 @@
+const path = require("path");
+
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development";
+
+console.log(`Using environment config: '${activeEnv}'`);
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+});
 module.exports = {
   plugins: [
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: path.join(__dirname, "src/pages"),
+      },
+    },
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
---
+++ src/pages/menu.js
@@ -0,0 +1,39 @@
+import { graphql } from "gatsby";
+import React from "react";
+
+export default function Menu(props) {
+  const { data } = props;
+  console.log("propsssssssss", props);
+  return (
+    <div>
+      <h1>Hello, from the menu!</h1>
+      {data.allContentfulNavbarThemeItems.nodes.map(
+        (item) => item.menuItemText + " ,"
+      )}
+    </div>
+  );
+}
+
+export const query = graphql`
+  query {
+    allContentfulNavbarThemeItems(filter: { node_locale: { eq: "en-US" } }) {
+      nodes {
+        checkLoginStatus
+        hasSubMenu
+        location
+        menuItemText
+        subMenuColumns
+        subMenuItems {
+          id
+          sectionId
+          subTitle
+          subMenuLinks {
+            id
+            name
+            route
+          }
+        }
+      }
+    }
+  }
+`;
